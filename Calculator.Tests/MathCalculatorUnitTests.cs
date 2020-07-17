86186bc091d35bdcfe30a96ce5a914984d07e35d
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 18:57:12 +0500

    Add mathematical calculator feature

---
+++ Calculator.Tests/MathCalculatorUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using Calculator.Interfaces;
+using Calculator.MathOperations;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class MathCalculatorUnitTests
+    {
+        private readonly MathCalculator _calculator;
+
+        private Mock<IParser> _mockParser;
+        private Mock<INotationConverter> _mockNotationConverter;
+        private Mock<IMathProcessor> _mockMathProcessor;
+
+        private readonly MathOperation[] _mathOperations;
+
+        public MathCalculatorUnitTests()
+        {
+            _mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
+            _mockParser = new Mock<IParser>();
+            _mockNotationConverter = new Mock<INotationConverter>();
+            _mockMathProcessor = new Mock<IMathProcessor>();
+
+            _calculator = new MathCalculator(_mockParser.Object, _mockNotationConverter.Object, _mockMathProcessor.Object, _mathOperations);
+        }
+
+        [Fact]
+        public void CalculatorCalculates()
+        {
+            const string inputExpression = "1+2*3";
+
+            var parsedExpression = new List<object>
+            {
+                1.00,
+                new AddMathOperation(),
+                2.00,
+                new MulMathOperation(),
+                3.00
+            };
+
+            var convertedExpression = new List<object>
+            {
+                1.00,
+                2.00,
+                3.00,
+                new MulMathOperation(),
+                new AddMathOperation()
+            };
+
+            const double expectedResult = 7;
+
+            _mockParser
+                .Setup(p => p.Parse(inputExpression, _mathOperations))
+                .Returns(parsedExpression);
+
+            _mockNotationConverter
+                .Setup(nc => nc.ConvertToReversePolishNotation(parsedExpression))
+                .Returns(convertedExpression);
+
+            _mockMathProcessor
+                .Setup(mp => mp.Process(convertedExpression))
+                .Returns(expectedResult);
+
+            var actualResult = _calculator.Calculate(inputExpression);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Calculator/MathCalculator.cs
@@ -0,0 +1,45 @@
+﻿using Calculator.Interfaces;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Mathematical calculator
+    /// </summary>
+    public class MathCalculator
+    {
+        private readonly MathOperation[] _operations;
+
+        private readonly IParser _parser;
+        private readonly INotationConverter _notationConverter;
+        private readonly IMathProcessor _mathProcessor;
+
+        /// <summary>
+        /// Mathematical calculator constructor with dependency injection
+        /// </summary>
+        /// <param name="parser">Parser of mathematical expression string</param>
+        /// <param name="notationConverter">Converter for different mathematical notation</param>
+        /// <param name="mathProcessor">Processor for performing mathematical operations</param>
+        /// <param name="operations">Mathematical operations</param>
+        public MathCalculator(IParser parser, INotationConverter notationConverter, IMathProcessor mathProcessor, MathOperation[] operations)
+        {
+            _parser = parser;
+            _notationConverter = notationConverter;
+            _mathProcessor = mathProcessor;
+            _operations = operations;
+        }
+
+        /// <summary>
+        /// Calculate a mathematical expression
+        /// </summary>
+        /// <param name="expression">Mathematical expression</param>
+        /// <returns>The result of the calculation</returns>
+        public double Calculate(string expression)
+        {
+            var parsedExpression = _parser.Parse(expression, _operations);
+
+            var convertedExpression = _notationConverter.ConvertToReversePolishNotation(parsedExpression);
+
+            return _mathProcessor.Process(convertedExpression);
+        }
+    }
+}
