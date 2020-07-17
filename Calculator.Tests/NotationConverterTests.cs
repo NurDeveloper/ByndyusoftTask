8b1d3dba2f016deeafae210a9b40f62f1121560d
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 17:47:12 +0500

    Add convert to reverse polish notation feature

--- Calculator.Tests/NotationConverterTests.cs
+++ Calculator.Tests/NotationConverterTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using Calculator.MathOperations;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Calculator.Tests
 {
@@ -8,6 +10,83 @@ namespace Calculator.Tests
         public void NotationConverter_should_be()
         {
             var notationConverter = new NotationConverter();
+        }
+
+        [Fact]
+        public void NotationConverter_converts_one_digit_to_RPN()
+        {
+            var notationConverter = new NotationConverter();
+            var inputValue = new List<object>
+            {
+                2.00
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue);
+
+            Assert.Collection(result, item => Assert.Equal(2.00, item));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_two_digit_and_operation_to_RPN()
+        {
+            var notationConverter = new NotationConverter();
+            var inputValue = new List<object>()
+            {
+                3.00,
+                new AddMathOperation(),
+                2.00
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue);
+
+            Assert.Collection(result,
+                item => Assert.Equal(3.00, item),
+                item => Assert.Equal(2.00, item),
+                item => Assert.Equal("+", item.ToString()));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_expression_with_first_priority_operation_to_RPN()
+        {
+            var notationConverter = new NotationConverter();
+            var inputValue = new List<object>()
+            {
+                4.00,
+                new AddMathOperation(),
+                2.00,
+                new MulMathOperation(),
+                3.00
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue);
+
+            Assert.Collection(result,
+                item => Assert.Equal(4.00, item),
+                item => Assert.Equal(2.00, item),
+                item => Assert.Equal(3.00, item),
+                item => Assert.Equal("*", item.ToString()),
+                item => Assert.Equal("+", item.ToString()));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_two_digit_and_operation_with_brackets_to_RPN()
+        {
+            var notationConverter = new NotationConverter();
+            var inputValue = new List<object>()
+            {
+                new LeftBracketMathOperation(),
+                3.00,
+                new AddMathOperation(),
+                2.00,
+                new RightBracketMathOperation()
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue);
+
+            Assert.Collection(result,
+                item => Assert.Equal(3.00, item),
+                item => Assert.Equal(2.00, item),
+                item => Assert.Equal("+", item.ToString()));
         }
     }
 }
--- Calculator/NotationConverter.cs
+++ Calculator/NotationConverter.cs
@@ -1,9 +1,68 @@
-﻿namespace Calculator
+﻿using Calculator.MathOperations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
 {
     /// <summary>
     /// Converter for different mathematical notation
     /// </summary>
     public class NotationConverter
     {
+        /// <summary>
+        /// Convert infix notation to reverse polish notation
+        /// </summary>
+        /// <param name="infixNotation">Infix notation</param>
+        /// <returns>Reverse polish notation</returns>
+        public IEnumerable<object> ConvertToReversePolishNotation(IEnumerable<object> infixNotation)
+        {
+            var stack = new Stack<MathOperation>();
+            var result = new List<object>();
+
+            foreach (var item in infixNotation)
+            {
+                if (item is double)
+                {
+                    result.Add(item);
+
+                    continue;
+                }
+
+                if (item is LeftBracketMathOperation leftBracketMathOperation)
+                {
+                    stack.Push(leftBracketMathOperation);
+
+                    continue;
+                }
+
+                if (item is RightBracketMathOperation rightBracketMathOperation)
+                {
+                    while (!(stack.Peek() is LeftBracketMathOperation))
+                    {
+                        result.Add(stack.Pop());
+                    }
+
+                    stack.Pop();
+
+                    continue;
+                }
+
+                var operation = item as MathOperation;
+
+                while (stack.Count() != 0 && stack.Peek().Priority > operation.Priority)
+                {
+                    result.Add(stack.Pop());
+                }
+
+                stack.Push((MathOperation)item);
+            }
+
+            while (stack.Count() != 0)
+            {
+                result.Add(stack.Pop());
+            }
+
+            return result;
+        }
     }
 }
