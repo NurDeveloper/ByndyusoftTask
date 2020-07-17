d716b2a204d6a162b4a2b3412e9f06874e9d0710
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 19:21:21 +0500

    Add console application client of libraries

---
+++ CalculatorConsoleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using Calculator;
+using Calculator.Interfaces;
+using Calculator.MathOperations;
+using System;
+
+namespace CalculatorConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
+            var parser = new Parser();
+            var notationConverter = new NotationConverter();
+            var mathProcessor = new MathProcessor();
+
+            var calculator = new MathCalculator(parser, notationConverter, mathProcessor, mathOperations);
+
+            Console.WriteLine("Calculator");
+            Console.WriteLine("To complete the work, type exit.");
+            while (true)
+            {
+                Console.Write("Enter a math expression: ");
+                var expression = Console.ReadLine();
+
+                if (expression == "exit")
+                {
+                    Console.WriteLine("Work completed.");
+                    break;
+                }
+
+                var result = calculator.Calculate(expression);
+                Console.WriteLine("Result: {0}", result);
+
+            }
+        }
+    }
+}
