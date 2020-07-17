b3a1a5dc977817624dde7c545ec8c576287b5abe
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 11:28:07 +0500

    Add math expression parser

---
+++ Calculator.Tests/ParserTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Calculator.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void Parser_should_be()
+        {
+            var parser = new Parser();
+        }
+    }
+}
---
+++ Calculator/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Parser of mathematical expression string
+    /// </summary>
+    public class Parser
+    {
+    }
+}
