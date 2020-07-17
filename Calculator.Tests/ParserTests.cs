b22cb97224fe7287da8777d1847f9074339a9182
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 14:13:13 +0500

    Add parse feature

--- Calculator.Tests/ParserTests.cs
+++ Calculator.Tests/ParserTests.cs
@@ -1,13 +1,89 @@
-﻿using Xunit;
+﻿using Calculator.MathOperations;
+using Xunit;
 
 namespace Calculator.Tests
 {
     public class ParserTests
     {
+        private readonly MathOperation[] _mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
         [Fact]
         public void Parser_should_be()
         {
             var parser = new Parser();
+        }
+
+        [Fact]
+        public void Parser_parses_one_digit()
+        {
+            var parser = new Parser();
+            string inputValue = "2";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result, item => Assert.Equal(2.00, item));
+        }
+
+        [Fact]
+        public void Parser_parses_two_digit()
+        {
+            var parser = new Parser();
+            string inputValue = "23";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result, item => Assert.Equal(23.00, item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_with_dot()
+        {
+            var parser = new Parser();
+            string inputValue = "23.5";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result, item => Assert.Equal(23.50, item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_and_operation()
+        {
+            var parser = new Parser();
+            string inputValue = "3+2";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => Assert.Equal(3.00, item),
+                item => Assert.Equal("+", item.ToString()),
+                item => Assert.Equal(2.00, item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_and_operation_with_branches()
+        {
+            var parser = new Parser();
+            string inputValue = "3.1*(2.7+5)";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => Assert.Equal(3.10, item),
+                item => Assert.Equal("*", item.ToString()),
+                item => Assert.Equal("(", item.ToString()),
+                item => Assert.Equal(2.70, item),
+                item => Assert.Equal("+", item.ToString()),
+                item => Assert.Equal(5.00, item),
+                item => Assert.Equal(")", item.ToString()));
         }
     }
 }
--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Calculator.MathOperations;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace Calculator
@@ -9,5 +11,59 @@ namespace Calculator
     /// </summary>
     public class Parser
     {
+        /// <summary>
+        /// Parse string to terms of mathematical expression
+        /// </summary>
+        /// <param name="expression">Input expression</param>
+        /// <param name="mathOperations">Avaliable math operations</param>
+        /// <returns></returns>
+        public IEnumerable<object> Parse(string expression, MathOperation[] mathOperations)
+        {
+            var result = new List<object>();
+
+            double value = 0;
+
+            var valueBuilder = new StringBuilder();
+
+            for (var i = 0; i < expression.Length; i++)
+            {
+                if (TryParseMathOperation(expression[i].ToString(), mathOperations, out MathOperation mathOperation))
+                {
+                    ParseValue(result, valueBuilder, out value);
+
+                    result.Add(mathOperation);
+
+                    continue;
+                }
+
+                valueBuilder.Append(expression[i]);
+            }
+
+            if (valueBuilder.Length != 0)
+            {
+                ParseValue(result, valueBuilder, out value);
+            }
+
+            return result;
+        }
+
+        private static void ParseValue(List<object> mathExpression, StringBuilder valueBuilder, out double value)
+        {
+            var valueAsString = valueBuilder.ToString();
+
+            if (double.TryParse(valueAsString, NumberStyles.Any, CultureInfo.InvariantCulture, out value))
+            {
+                mathExpression.Add(value);
+
+                valueBuilder.Clear();
+            }
+        }
+
+        private static bool TryParseMathOperation(string expressionItem, MathOperation[] mathOperations, out MathOperation mathOperation)
+        {
+            mathOperation = mathOperations.FirstOrDefault(m => m.ToString() == expressionItem);
+
+            return mathOperation != null;
+        }
     }
 }
