0c8db90b6ca787105183eebf48ae181d727e6509
Никита Нур <nikita.nur@hotmail.com>
2020-07-17 18:11:11 +0500

    Add mathematical processor feature

--- Calculator.Tests/MathProcessorUnitTests.cs
+++ Calculator.Tests/MathProcessorUnitTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using Calculator.MathOperations;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Calculator.Tests
 {
@@ -8,6 +10,103 @@ namespace Calculator.Tests
         public void MathProcessor_should_be()
         {
             var mathProcessor = new MathProcessor();
+        }
+
+        [Fact]
+        public void MathProcessor_returns_one_number()
+        {
+            var inputExpression = new List<object>()
+            {
+                2.00
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(2.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_add_operation()
+        {
+            var inputExpression = new List<object>()
+            {
+                2.50,
+                3.25,
+                new AddMathOperation()
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(5.75, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_sub_operation()
+        {
+            var inputExpression = new List<object>()
+            {
+                6.00,
+                2.00,
+                new SubMathOperation()
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(4.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_mul_operation()
+        {
+            var inputExpression = new List<object>()
+            {
+                6.00,
+                2.00,
+                new MulMathOperation()
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(12.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_div_operation()
+        {
+            var inputExpression = new List<object>()
+            {
+                6.00,
+                2.00,
+                new DivMathOperation()
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(3.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_several_math_operation()
+        {
+
+            var inputExpression = new List<object>()
+            {
+                2.00,
+                3.00,
+                5.00,
+                new MulMathOperation(),
+                new AddMathOperation()
+            };
+
+            var _mathProcessor = new MathProcessor();
+            var result = _mathProcessor.Process(inputExpression);
+
+            Assert.Equal(17.00, result);
         }
     }
 }
--- Calculator/MathProcessor.cs
+++ Calculator/MathProcessor.cs
@@ -1,9 +1,37 @@
-﻿namespace Calculator
+﻿using Calculator.MathOperations;
+using System.Collections.Generic;
+
+namespace Calculator
 {
     /// <summary>
     /// Processor for performing mathematical operations
     /// </summary>
     public class MathProcessor
     {
+        /// <summary>
+        /// Execute operations in a mathematical expression
+        /// </summary>
+        /// <param name="expression">Mathematical expression</param>
+        /// <returns>Execution result</returns>
+        public double Process(IEnumerable<object> expression)
+        {
+            var stack = new Stack<double>();
+
+            foreach (var item in expression)
+            {
+                if (item is double)
+                {
+                    stack.Push((double)item);
+                    continue;
+                }
+
+                if (item is IOperation operation)
+                {
+                    operation.Operate(stack);
+                }
+            }
+
+            return stack.Peek();
+        }
     }
 }
