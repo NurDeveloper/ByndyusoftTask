ae40c9edfbd0f18009ff5785fbf9b599b57af7e9
Никита Нур <nikita.nur@hotmail.com>
2020-07-22 01:26:08 +0500

    Added domain integration in parser

--- Calculator.Tests/ParserTests.cs
+++ Calculator.Tests/ParserTests.cs
@@ -1,4 +1,5 @@
-﻿using Calculator.Interfaces;
+﻿using Calculator.Domain.ExpressionUnits;
+using Calculator.Interfaces;
 using Calculator.MathOperations;
 using System;
 using Xunit;
@@ -109,6 +110,122 @@ namespace Calculator.Tests
             var result = parser.Parse(inputValue, _mathOperations);
 
             Assert.Collection(result, item => Assert.Equal(23.50, item));
+        }
+    }
+
+    public class ParserSupportingCalculatorDomainTests
+    {
+        private readonly MathOperation[] _mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
+        private static void AssertExpressionUnitEqual(ExpressionUnit expectedItem, ExpressionUnit item)
+        {
+            Assert.Equal(expectedItem.Type, item.Type);
+            Assert.Equal(expectedItem.Value, item.Value);
+        }
+
+        [Fact]
+        public void Parser_should_be()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+        }
+
+        [Fact]
+        public void Parser_parses_one_digit()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "2";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            var expectedItem = new NumberExpressionUnit("2");
+            Assert.Collection(result, item => AssertExpressionUnitEqual(expectedItem, item));
+        }
+
+        [Fact]
+        public void Parser_parses_two_digit()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "23";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            var expectedItem = new NumberExpressionUnit("23");
+            Assert.Collection(result, item => AssertExpressionUnitEqual(expectedItem, item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_with_dot()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "23.5";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            var expectedItem = new NumberExpressionUnit("23.5");
+            Assert.Collection(result, item => AssertExpressionUnitEqual(expectedItem, item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_and_operation()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "3+2";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("3"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("+"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("2"), item));
+        }
+
+        [Fact]
+        public void Parser_parses_digit_and_operation_with_branches()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "3.1*(2.7+5)";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("3.1"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("*"), item),
+                item => AssertExpressionUnitEqual(new LeftBracketExpressionUnit(), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("2.7"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("+"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("5"), item),
+                item => AssertExpressionUnitEqual(new RightBracketExpressionUnit(), item));
+        }
+
+        [Fact]
+        public void Parser_throws_custom_exception_for_wrong_input_value()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "5+b";
+
+            Action action = () => parser.Parse(inputValue, _mathOperations);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Invalid mathematical expression or unsupported operation for parsing.", exception.Message);
+        }
+
+        [Fact]
+        public void Parser_correct_parses_digit_with_comma()
+        {
+            var parser = new ParserSupportingCalculatorDomain();
+            string inputValue = "23,5";
+
+            var result = parser.Parse(inputValue, _mathOperations);
+
+            Assert.Collection(result, item => AssertExpressionUnitEqual(new NumberExpressionUnit("23.5"), item));
         }
     }
 }
--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -1,4 +1,6 @@
-﻿using Calculator.Interfaces;
+﻿using Calculator.Domain.ExpressionUnits;
+using Calculator.Interfaces;
+using Calculator.MathOperations;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -71,6 +73,96 @@ namespace Calculator
             mathOperation = mathOperations.FirstOrDefault(m => m.ToString() == expressionItem);
 
             return mathOperation != null;
+        }
+    }
+
+    /// <summary>
+    /// Parser of mathematical expression string
+    /// </summary>
+    public class ParserSupportingCalculatorDomain
+    {
+        /// <summary>
+        /// Parse string to terms of mathematical expression
+        /// </summary>
+        /// <param name="expression">Input expression</param>
+        /// <param name="mathOperations">Avaliable math operations</param>
+        /// <returns>Parsed mathematical expression</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        public IEnumerable<ExpressionUnit> Parse(string expression, MathOperation[] mathOperations)
+        {
+            var result = new List<ExpressionUnit>();
+
+            var valueBuilder = new StringBuilder();
+
+            for (var i = 0; i < expression.Length; i++)
+            {
+                if (TryParseMathOperation(expression[i].ToString(), mathOperations, out ExpressionUnit mathOperation))
+                {
+                    ParseValue(result, valueBuilder);
+
+                    result.Add(mathOperation);
+
+                    continue;
+                }
+
+                valueBuilder.Append(expression[i]);
+            }
+
+            ParseValue(result, valueBuilder);
+
+            return result;
+        }
+
+        private static void ParseValue(List<ExpressionUnit> mathExpression, StringBuilder valueBuilder)
+        {
+            if (valueBuilder.Length == 0)
+            {
+                return;
+            }
+
+            var valueAsString = valueBuilder.ToString().Replace(',', '.');
+
+            if (double.TryParse(valueAsString, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))
+            {
+                mathExpression.Add(new NumberExpressionUnit(valueAsString));
+
+                valueBuilder.Clear();
+            }
+            else
+            {
+                throw new ArgumentException("Invalid mathematical expression or unsupported operation for parsing.");
+            }
+        }
+
+        private static bool TryParseMathOperation(string expressionItem, MathOperation[] mathOperations, out ExpressionUnit expressionUnit)
+        {
+            var mathOperation = mathOperations.FirstOrDefault(m => m.ToString() == expressionItem);
+
+            if (mathOperation != null)
+            {
+                if (mathOperation is LeftBracketMathOperation)
+                {
+                    expressionUnit = new LeftBracketExpressionUnit();
+
+                    return true;
+                }
+
+                if (mathOperation is RightBracketMathOperation)
+                {
+                    expressionUnit = new RightBracketExpressionUnit();
+
+                    return true;
+                }
+
+                expressionUnit = new OperationExpressionUnit(mathOperation.ToString());
+
+                return true;
+            }
+
+            expressionUnit = null;
+
+            return false;
         }
     }
 }
