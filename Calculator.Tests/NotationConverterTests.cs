eb6b3cb51bdca01ba44267bd1b1d48643c278130
Никита Нур <nikita.nur@hotmail.com>
2020-07-22 18:39:59 +0500

    Added domain integration in notation converter

--- Calculator.Tests/NotationConverterTests.cs
+++ Calculator.Tests/NotationConverterTests.cs
@@ -1,4 +1,6 @@
-﻿using Calculator.MathOperations;
+﻿using Calculator.Domain.ExpressionUnits;
+using Calculator.Interfaces;
+using Calculator.MathOperations;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -106,6 +108,144 @@ namespace Calculator.Tests
 
             var exception = Assert.Throws<ArgumentException>(action);
             Assert.Equal("Invalid mathematical expression or non-infix notation.", exception.Message);
+        }
+    }
+
+    public class NotationConverterSupportingCalculatorDomainTests
+    {
+        private readonly MathOperation[] _mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
+        private static void AssertExpressionUnitEqual(ExpressionUnit expectedItem, ExpressionUnit item)
+        {
+            Assert.Equal(expectedItem.Type, item.Type);
+            Assert.Equal(expectedItem.Value, item.Value);
+        }
+
+        [Fact]
+        public void NotationConverter_should_be()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+        }
+
+        [Fact]
+        public void NotationConverter_converts_one_digit_to_RPN()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>
+            {
+                new NumberExpressionUnit("2.00")
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            var expectedItem = new NumberExpressionUnit("2.00");
+            Assert.Collection(result, item => AssertExpressionUnitEqual(expectedItem, item));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_two_digit_and_operation_to_RPN()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("3.00"),
+                new OperationExpressionUnit("+"),
+                new NumberExpressionUnit("2.00")
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("3.00"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("2.00"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("+"), item));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_expression_with_first_priority_operation_to_RPN()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("4.00"),
+                new OperationExpressionUnit("+"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("*"),
+                new NumberExpressionUnit("3.00")
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("4.00"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("2.00"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("3.00"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("*"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("+"), item));
+        }
+
+        [Fact]
+        public void NotationConverter_converts_two_digit_and_operation_with_brackets_to_RPN()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>()
+            {
+                new LeftBracketExpressionUnit(),
+                new NumberExpressionUnit("3.00"),
+                new OperationExpressionUnit("+"),
+                new NumberExpressionUnit("2.00"),
+                new RightBracketExpressionUnit()
+            };
+
+            var result = notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            Assert.Collection(result,
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("3.00"), item),
+                item => AssertExpressionUnitEqual(new NumberExpressionUnit("2.00"), item),
+                item => AssertExpressionUnitEqual(new OperationExpressionUnit("+"), item));
+        }
+
+        [Fact]
+        public void NotationConverter_throws_custom_exception_for_wrong_brackets()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>()
+            {
+                new RightBracketExpressionUnit(),
+                new NumberExpressionUnit("3.00"),
+                new OperationExpressionUnit("+"),
+                new NumberExpressionUnit("2.00")
+             };
+
+            Action action = () => notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Invalid mathematical expression or non-infix notation.", exception.Message);
+        }
+
+        [Fact]
+        public void NotationConverter_throws_custom_exception_for_unsupported_operation()
+        {
+            var notationConverter = new NotationConverterSupportingCalculatorDomain();
+            var inputValue = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("3.00"),
+                new OperationExpressionUnit("&"),
+                new NumberExpressionUnit("2.00")
+             };
+
+            Action action = () => notationConverter.ConvertToReversePolishNotation(inputValue, _mathOperations);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Invalid mathematical expression or unsupported operation.", exception.Message);
         }
     }
 }
--- Calculator/NotationConverter.cs
+++ Calculator/NotationConverter.cs
@@ -1,4 +1,5 @@
-﻿using Calculator.Interfaces;
+﻿using Calculator.Domain.ExpressionUnits;
+using Calculator.Interfaces;
 using Calculator.MathOperations;
 using System;
 using System.Collections.Generic;
@@ -80,6 +81,96 @@ namespace Calculator
             }
 
             return result;
+        }
+    }
+
+    /// <summary>
+    /// Converter for different mathematical notation
+    /// </summary>
+    public class NotationConverterSupportingCalculatorDomain
+    {
+        /// <summary>
+        /// Convert infix notation to reverse polish notation
+        /// </summary>
+        /// <param name="infixNotation">Infix notation</param>
+        /// <param name="mathOperations">Avaliable math operations</param>
+        /// <returns>Reverse polish notation</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        public IEnumerable<ExpressionUnit> ConvertToReversePolishNotation(IEnumerable<ExpressionUnit> infixNotation, MathOperation[] mathOperations)
+        {
+            var stack = new Stack<ExpressionUnit>();
+            var result = new List<ExpressionUnit>();
+
+            foreach (var item in infixNotation)
+            {
+                if (item.Type == Domain.Enums.ExpressionUnitType.Number)
+                {
+                    result.Add(item);
+
+                    continue;
+                }
+
+                if (item.Type == Domain.Enums.ExpressionUnitType.LeftBracket)
+                {
+                    stack.Push(item);
+
+                    continue;
+                }
+
+                if (item.Type == Domain.Enums.ExpressionUnitType.RightBracket)
+                {
+                    while (stack.Count != 0 && !(stack.Peek().Type == Domain.Enums.ExpressionUnitType.LeftBracket))
+                    {
+                        result.Add(stack.Pop());
+                    }
+
+                    if (stack.Count == 0)
+                    {
+                        throw new ArgumentException("Invalid mathematical expression or non-infix notation.");
+                    }
+
+                    stack.Pop();
+
+                    continue;
+                }
+
+                MathOperation operation = CreateOperationByKeyword(item, mathOperations);
+
+                while (stack.Count() != 0 && CreateOperationByKeyword(stack.Peek(), mathOperations).Priority > operation.Priority)
+                {
+                    result.Add(stack.Pop());
+                }
+
+                stack.Push(item);
+            }
+
+            while (stack.Count() != 0)
+            {
+                var peek = stack.Peek();
+                if ((peek.Type == Domain.Enums.ExpressionUnitType.Operation) && !(stack.Peek().Type == Domain.Enums.ExpressionUnitType.LeftBracket))
+                {
+                    result.Add(stack.Pop());
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid mathematical expression or non-infix notation.");
+                }
+            }
+
+            return result;
+        }
+
+        private static MathOperation CreateOperationByKeyword(ExpressionUnit item, MathOperation[] mathOperations)
+        {
+            var operation = mathOperations.FirstOrDefault(m => m.ToString() == item.Value);
+
+            if (operation == null)
+            {
+                throw new ArgumentException("Invalid mathematical expression or unsupported operation.");
+            }
+
+            return operation;
         }
     }
 }
