cdae8fabd455b559ae1e6a1c7011e579876f382f
Никита Нур <nikita.nur@hotmail.com>
2020-07-22 19:11:33 +0500

    Added domain integration in math processor

--- Calculator.Tests/MathProcessorUnitTests.cs
+++ Calculator.Tests/MathProcessorUnitTests.cs
@@ -1,4 +1,6 @@
-﻿using Calculator.MathOperations;
+﻿using Calculator.Domain.ExpressionUnits;
+using Calculator.Interfaces;
+using Calculator.MathOperations;
 using System;
 using System.Collections.Generic;
 using Xunit;
@@ -142,6 +144,180 @@ namespace Calculator.Tests
 
             var exception = Assert.Throws<ArgumentException>(action);
             Assert.Equal("Invalid mathematical expression.", exception.Message);
+        }
+    }
+
+    public class MathProcessorUnitSupportingCalculatorDomainTests
+    {
+        private readonly MathOperation[] _mathOperations = new MathOperation[]
+            {
+                new AddMathOperation(),
+                new SubMathOperation(),
+                new MulMathOperation(),
+                new DivMathOperation(),
+                new LeftBracketMathOperation(),
+                new RightBracketMathOperation()
+            };
+
+        [Fact]
+        public void MathProcessor_should_be()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+        }
+
+        [Fact]
+        public void MathProcessor_returns_one_number()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("2.00")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(2.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_add_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("2.50"),
+                new NumberExpressionUnit("3.25"),
+                new OperationExpressionUnit("+")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(5.75, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_sub_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("6.00"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("-")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(4.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_mul_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("6.00"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("*")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(12.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_div_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("6.00"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("/")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(3.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_result_of_several_math_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("2.00"),
+                new NumberExpressionUnit("3.00"),
+                new NumberExpressionUnit("5.00"),
+                new OperationExpressionUnit("*"),
+                new OperationExpressionUnit("+")
+            };
+
+            var result = mathProcessor.Process(inputExpression, _mathOperations);
+
+            Assert.Equal(17.00, result);
+        }
+
+        [Fact]
+        public void MathProcessor_returns_divide_by_zero_exception()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("3.00"),
+                new NumberExpressionUnit("0.00"),
+                new OperationExpressionUnit("/")
+            };
+
+            Action action = () => mathProcessor.Process(inputExpression, _mathOperations);
+
+            var exception = Assert.Throws<DivideByZeroException>(action);
+            Assert.Equal("You cannot divide by zero.", exception.Message);
+        }
+
+        [Fact]
+        public void MathProcessor_should_throw_exception_for_operation_without_arg()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("3.00"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("*"),
+                new OperationExpressionUnit("+")
+            };
+
+            Action action = () => mathProcessor.Process(inputExpression, _mathOperations);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Invalid mathematical expression.", exception.Message);
+        }
+
+        [Fact]
+        public void MathProcessor_should_throw_custom_exception_for_unsupported_operation()
+        {
+            var mathProcessor = new MathProcessorSupportingCalculatorDomain();
+
+            var inputExpression = new List<ExpressionUnit>()
+            {
+                new NumberExpressionUnit("3.00"),
+                new NumberExpressionUnit("2.00"),
+                new OperationExpressionUnit("$")
+            };
+
+            Action action = () => mathProcessor.Process(inputExpression, _mathOperations);
+
+            var exception = Assert.Throws<ArgumentException>(action);
+            Assert.Equal("Invalid mathematical expression or unsupported operation.", exception.Message);
         }
     }
 }
--- Calculator/MathProcessor.cs
+++ Calculator/MathProcessor.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Calculator.Interfaces;
 using System.Collections.Generic;
+using Calculator.Domain.ExpressionUnits;
+using System.Globalization;
+using System.Linq;
 
 namespace Calculator
 {
@@ -36,6 +39,56 @@ namespace Calculator
             }
 
             return stack.Peek();
+        }
+    }
+
+    /// <summary>
+    /// Processor for performing mathematical operations
+    /// </summary>
+    public class MathProcessorSupportingCalculatorDomain
+    {
+        /// <summary>
+        /// Execute operations in a mathematical expression
+        /// </summary>
+        /// <param name="expression">Mathematical expression</param>
+        /// <param name="mathOperations">Avaliable math operations</param>
+        /// <returns>Execution result</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="DivideByZeroException"></exception>
+        /// <exception cref="NullReferenceException"></exception>
+        public double Process(IEnumerable<ExpressionUnit> expression, MathOperation[] mathOperations)
+        {
+            var stack = new Stack<double>();
+
+            foreach (var item in expression)
+            {
+                if (item.Type == Domain.Enums.ExpressionUnitType.Number)
+                {
+                    stack.Push(double.Parse(item.Value, NumberStyles.Any, CultureInfo.InvariantCulture));
+                    continue;
+                }
+
+                if (item.Type == Domain.Enums.ExpressionUnitType.Operation)
+                {
+                    var operation = CreateOperationByKeyword(item, mathOperations);
+
+                    operation.Operate(stack);
+                }
+            }
+
+            return stack.Peek();
+        }
+
+        private static IOperation CreateOperationByKeyword(ExpressionUnit item, MathOperation[] mathOperations)
+        {
+            var operation = mathOperations.FirstOrDefault(m => m.ToString() == item.Value);
+
+            if (operation == null)
+            {
+                throw new ArgumentException("Invalid mathematical expression or unsupported operation.");
+            }
+
+            return operation as IOperation;
         }
     }
 }
