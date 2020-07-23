529a52d55a41f117334c5d64435856e8af640914
Никита Нур <nikita.nur@hotmail.com>
2020-07-23 23:42:40 +0500

    Added operations container

---
+++ Calculator.Tests/MathOperationsContainerTests.cs
@@ -0,0 +1,158 @@
+﻿using Calculator.Domain.MathOperations;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    public class MathOperationsContainerTests
+    {
+        [Fact]
+        public void MathOperationsContainer_should_be()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_true_for_add_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+            var keyword = "+";
+
+            var result = mathOperationsContainer.ContainsOperation(keyword);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_true_for_sub_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+            var keyword = "-";
+
+            var result = mathOperationsContainer.ContainsOperation(keyword);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_true_for_mul_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+            var keyword = "*";
+
+            var result = mathOperationsContainer.ContainsOperation(keyword);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_true_for_div_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+            var keyword = "/";
+
+            var result = mathOperationsContainer.ContainsOperation(keyword);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_false_for_wrong_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+            var keyword = "&";
+
+            var result = mathOperationsContainer.ContainsOperation(keyword);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_add_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+
+            var keyword = "+";
+
+            var stack = new Stack<double>();
+            stack.Push(2.00);
+            stack.Push(3.00);
+
+            MathOperation mathOperation = mathOperationsContainer.GetOperationOrDefault(keyword);
+
+            mathOperation.Operate(stack);
+
+            Assert.Equal(1, mathOperation.Priority);
+            Assert.Equal(5.00, stack.Peek());
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_sub_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+
+            var keyword = "-";
+
+            var stack = new Stack<double>();
+            stack.Push(6.00);
+            stack.Push(4.00);
+
+            MathOperation mathOperation = mathOperationsContainer.GetOperationOrDefault(keyword);
+
+            mathOperation.Operate(stack);
+
+            Assert.Equal(1, mathOperation.Priority);
+            Assert.Equal(2.00, stack.Peek());
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_mul_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+
+            var keyword = "*";
+
+            var stack = new Stack<double>();
+            stack.Push(5.00);
+            stack.Push(5.00);
+
+            MathOperation mathOperation = mathOperationsContainer.GetOperationOrDefault(keyword);
+
+            mathOperation.Operate(stack);
+
+            Assert.Equal(2, mathOperation.Priority);
+            Assert.Equal(25.00, stack.Peek());
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_div_operation_by_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+
+            var keyword = "/";
+
+            var stack = new Stack<double>();
+            stack.Push(8.00);
+            stack.Push(4.00);
+
+            MathOperation mathOperation = mathOperationsContainer.GetOperationOrDefault(keyword);
+
+            mathOperation.Operate(stack);
+
+            Assert.Equal(2, mathOperation.Priority);
+            Assert.Equal(2.00, stack.Peek());
+        }
+
+        [Fact]
+        public void MathOperationsContainer_returns_default_operation_by_wrong_keyword()
+        {
+            var mathOperationsContainer = new MathOperationsContainer();
+
+            var keyword = "&";
+
+            MathOperation mathOperation = mathOperationsContainer.GetOperationOrDefault(keyword);
+
+            Assert.Null(mathOperation);
+        }
+    }
+}
---
+++ Calculator/MathOperationsContainer.cs
@@ -0,0 +1,47 @@
+﻿using Calculator.Domain.MathOperations;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Container for mathematical operations. It has the characteristics
+    /// of a Flyweight pattern
+    /// </summary>
+    public class MathOperationsContainer
+    {
+        private readonly Dictionary<string, MathOperation> _mathOperations;
+
+        public MathOperationsContainer()
+        {
+            _mathOperations = new Dictionary<string, MathOperation>
+            {
+                { "+", new AddMathOperation() },
+                { "-", new SubMathOperation() },
+                { "*", new MulMathOperation() },
+                { "/", new DivMathOperation() }
+            };
+        }
+
+        /// <summary>
+        /// Checking for the existence of an operation in a container
+        /// </summary>
+        /// <param name="keyword">Operation keyword</param>
+        /// <returns>If it contains then returns true</returns>
+        public bool ContainsOperation(string keyword)
+        {
+            return _mathOperations.ContainsKey(keyword);
+        }
+
+        /// <summary>
+        /// Getting a link to an operation by keyword
+        /// </summary>
+        /// <param name="keyword">Operation keyword</param>
+        /// <returns>Returns a link to the operation by keyword or default instance</returns>
+        public MathOperation GetOperationOrDefault(string keyword)
+        {
+            _mathOperations.TryGetValue(keyword, out MathOperation mathOperation);
+
+            return mathOperation;
+        }
+    }
+}
