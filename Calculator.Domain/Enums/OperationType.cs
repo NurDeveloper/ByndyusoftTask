78fd27d5d49b06f23feb5d4ab525c3cfd1554eb4
Никита Нур <nikita.nur@hotmail.com>
2020-07-24 22:48:43 +0500

    Added operation characteristics

---
+++ Calculator.Domain/Enums/OperationType.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator.Domain.Enums
+{
+    /// <summary>
+    /// Available types of operation
+    /// </summary>
+    public enum OperationType
+    {
+        /// <summary>
+        /// Unary operation type
+        /// </summary>
+        Unary,
+
+        /// <summary>
+        /// Binary operation type
+        /// </summary>
+        Binary
+    }
+}
---
+++ Calculator.Domain/OperationCharacteristics.cs
@@ -0,0 +1,32 @@
+﻿using Calculator.Domain.Enums;
+
+namespace Calculator.Domain
+{
+    /// <summary>
+    /// Characteristics of a mathematical operation
+    /// </summary>
+    public class OperationCharacteristics
+    {
+        /// <summary>
+        /// Constructor for characteristics of a mathematical operation
+        /// </summary>
+        /// <param name="value">String representation of a mathematical operation</param>
+        /// <param name="type">Type of operation</param>
+        public OperationCharacteristics(string value, OperationType type)
+        {
+            Value = value;
+            Type = type;
+        }
+
+        /// <summary>
+        /// String representation of a mathematical operation.
+        /// The candidate for the key word
+        /// </summary>
+        public string Value { get; }
+
+        /// <summary>
+        /// Type of operation
+        /// </summary>
+        public OperationType Type { get; }
+    }
+}
